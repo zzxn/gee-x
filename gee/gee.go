ad724ae492178ee7db847640df673ecf9c862edc
zzxn <zhuxn@shihe.tech>
2022-11-17 22:56:05 +0800

    day4: group control

--- gee/gee.go
+++ gee/gee.go
@@ -1,38 +1,66 @@
 package gee
 
 import (
+	"log"
 	"net/http"
+	"strings"
 )
 
 type HandlerFunc func(c *Context)
 
+type RouterGroup struct {
+    prefix string // prefix which this group handle
+    middlewares []HandlerFunc
+    engine *Engine // a group share engine instance with its parent
+}
+
 // Engine implements the interface of ServeHTTP
 type Engine struct {
+    *RouterGroup
 	router *router
+    groups []*RouterGroup  // store all groups under it
 }
 
 func New() *Engine {
-	return &Engine{
-		router: newRouter(),
-	}
+    engine := &Engine{router: newRouter()}
+    engine.RouterGroup = &RouterGroup{engine: engine}
+    engine.groups = []*RouterGroup{engine.RouterGroup}
+    return engine
+}
+
+func (this *RouterGroup) Group(prefix string) *RouterGroup {
+    engine := this.engine
+    if !strings.HasPrefix(prefix, "/") {
+        log.Panicf("group prefix must start with /, but found %s\n", prefix)
+    }
+    newGroup := &RouterGroup{
+        prefix: this.prefix + prefix, // so prefix must start with "/"
+        engine: engine,
+    }
+    engine.groups = append(engine.groups, newGroup)
+    return newGroup
+}
+
+func (this *RouterGroup) addRoute(method string, path string, handler HandlerFunc) {
+    if !strings.HasPrefix(path, "/") {
+        log.Panicf("router path must start with /, but found %s\n", path)
+    }
+    pattern := this.prefix + path
+    this.engine.router.addRoute(method, pattern, handler)
+}
+
+func (this *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	this.addRoute("GET", pattern, handler)
+}
+
+func (this *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	this.addRoute("POST", pattern, handler)
 }
 
 // implement http.Handler interface.
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c := newContext(w, req)
 	engine.router.handle(c)
-}
-
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
-}
-
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
-}
-
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
--- main.go
+++ main.go
@@ -10,29 +10,39 @@ import (
 func main() {
 	r := gee.New()
 
-	r.GET("/", func(c *gee.Context) {
-		c.HTML("<h1> Hello Gee</h1>")
+	r.GET("/index", func(c *gee.Context) {
+		c.HTML("<h1> GEE INDEX </h1>")
 	})
 
-	r.GET("/hello", func(c *gee.Context) {
-		// expect /hello?name=geekandrew
-		c.String("hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-
-	r.GET("/hello/:name", func(c *gee.Context) {
-		c.String("hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *gee.Context) {
-		c.JSON(gee.H{"filepath": c.Param("filepath")})
-	})
-
-	r.POST("/login", func(c *gee.Context) {
-		c.JSON(gee.H{
-			"username": c.Form("username"),
-			"password": c.Form("password"),
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *gee.Context) {
+			c.HTML("<h1> Hello Gee</h1>")
 		})
-	})
+
+		v1.GET("/hello", func(c *gee.Context) {
+			// expect /hello?name=geekandrew
+			c.String("hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *gee.Context) {
+			c.String("hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+
+		v2.GET("/assets/*filepath", func(c *gee.Context) {
+			c.JSON(gee.H{"filepath": c.Param("filepath")})
+		})
+
+		v2.POST("/login", func(c *gee.Context) {
+			c.JSON(gee.H{
+				"username": c.Form("username"),
+				"password": c.Form("password"),
+			})
+		})
+	}
 
 	r.Run(":9999")
 }
